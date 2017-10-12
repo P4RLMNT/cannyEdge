37f3f0d93de3fc55322a541e4b3d79cb46308321
P4RLMNT <andrewegates@gmail.com>
2017-10-12 15:01:06 -0400

    non-maximum suppression completed

--- canny.c
+++ canny.c
@@ -9,9 +9,11 @@
 #define  SIGTWO 2
 
 void imageToFile(char*, int[256][256]);
-void isCand(double image[][PICSIZE]);
+void hysteresis(double image[][PICSIZE]);
 void nonMaxSup();
 void normalize(double maxVal);
+void magToFile(char *file, double output[256][256]);
+int threshold(double pixel);
 
 
 
@@ -20,18 +22,17 @@ int outpicx[PICSIZE][PICSIZE];
 int outpicy[PICSIZE][PICSIZE];
 int    peaks[PICSIZE][PICSIZE]; // non-maximum suppression
 double xmask[MAXMASK][MAXMASK], ymask[MAXMASK][MAXMASK];
-double conv[PICSIZE][PICSIZE];
 double ival[PICSIZE][PICSIZE];
 double gradDir[PICSIZE][PICSIZE];
-int lowOutPic[PICSIZE][PICSIZE];
-int highOutPic[PICSIZE][PICSIZE];
+double thresh[PICSIZE][PICSIZE];
+int candidates[PICSIZE][PICSIZE];
 int final[PICSIZE][PICSIZE];
-double hThresh = 110.0, lThresh = 40.0;
+double hThresh = 110.0, lThresh = 30.0;
 
 
 int main(int argc, char** argv){
 	int     i,j,p,q,s,t,mr,centx,centy;
-	double  xmaskval,ymaskval,sum1,sum2,sig,maxival,minival,maxval,ZEROTOL;
+	double  xmaskval,ymaskval,Gx,Gy,sig,maxival;
 	FILE    *fo1, *fo2,*fp1, *fopen();
 	char    *foobar, *pgmType;
 	int picLength, picWidth;
@@ -40,10 +41,7 @@ int main(int argc, char** argv){
 	foobar = *argv;
 	fp1=fopen(foobar,"rb");
 
-
 	fo1=fopen("cannyMag.pgm","wb");
-
-
 	fo2=fopen("canny2output.pgm","wb");
 
 	argc--; argv++;
@@ -67,28 +65,28 @@ int main(int argc, char** argv){
 	for (p=-mr;p<=mr;p++)
 	{  for (q=-mr;q<=mr;q++)
 		 {
-				xmaskval = (p * (exp(-1*(((p*p)+(q*q))/(2*(sig*sig))))));
-				ymaskval = (q * (exp(-1*(((p*p)+(q*q))/(2*(sig*sig))))));
-				(xmask[p+centy][q+centx]) = xmaskval;
-				(ymask[p+centy][q+centx]) = ymaskval;
+				xmaskval = (q * (exp(-1*(((p*p)+(q*q))/(2*(sig*sig))))));
+				ymaskval = (p * (exp(-1*(((p*p)+(q*q))/(2*(sig*sig))))));
+				xmask[p+centy][q+centx] = xmaskval;
+				ymask[p+centy][q+centx] = ymaskval;
 		 }
 	}
 	for (i=mr;i<=255-mr;i++)
 	{ for (j=mr;j<=255-mr;j++)
 		{
-			 sum1 = 0;
-			 sum2 = 0;
+			 Gx = 0.0;
+			 Gy = 0.0;
 			 for (p=-mr;p<=mr;p++)
 			 {
 					for (q=-mr;q<=mr;q++)
 					{
-						 sum1 += pic[i+p][j+q] * ymask[p+centy][q+centx];
-						 sum2 += pic[i+p][j+q] * xmask[p+centy][q+centx];
+						 Gx += pic[i+p][j+q] * xmask[p+centy][q+centx];
+						 Gy += pic[i+p][j+q] * ymask[p+centy][q+centx];
 					}
 			 }
-			 outpicx[i][j] = (int)sum1;
-			 outpicy[i][j] = (int)sum2;
-			 gradDir[i][j] = atan(sum2/sum1);
+			 outpicx[i][j] = (int)Gx;
+			 outpicy[i][j] = (int)Gy;
+			 gradDir[i][j] = (atan2(Gx,Gy)/M_PI) * 180.0; // calculating gradient direction
 		}
 	}
 // sobel code
@@ -107,34 +105,27 @@ for (i=mr;i<256-mr;i++)
 
 normalize(maxival);
 nonMaxSup();
-// printing magnitude imageToFile
 
-fprintf(fo1,"P5\n256 256\n255\n");  /* Print out the .pgm header */
-for (i=0;i<256;i++)
-{ for (j=0;j<256;j++)
-		{
-		 // applying high threshold
-		 if(ival[i][j] > hThresh) highOutPic[i][j] = 255;
-		 else highOutPic[i][j] = 0;
-		 // applying low threshold
-		 if(ival[i][j] > lThresh) lowOutPic[i][j] = 255;
-		 else lowOutPic[i][j] = 0;
-		 fprintf(fo1,"%c",(char)((int)(ival[i][j])));
-
-		}
-}
-
+magToFile("cannyMag.pgm", ival);
 imageToFile("cannyXGrad.pgm", outpicx);
 imageToFile("cannyYGrad.pgm", outpicy);
 imageToFile("cannyPeaks.pgm", peaks);
+magToFile("cannyThres.pgm", thresh);
 
  return 0;
 } // end main
 
-void isCand(double image[][PICSIZE])
+void hysteresis(double image[][PICSIZE])
 {
 
 }
+
+int threshold(double pixel)
+{
+	if(pixel > hThresh) return 1;
+	else if(pixel > lThresh) return 0;
+	else return -1;
+} // end threshold
 
 void nonMaxSup()
 {
@@ -144,48 +135,51 @@ void nonMaxSup()
 	{
 		peaks[i][0] = 0; // left edge
 		peaks[i][PICSIZE-1] = 0; // right edge
+
+		thresh[i][0] = 0.0; // left edge
+		thresh[i][PICSIZE-1] = 0.0; // right edge
 	}
 	for(j = 0; j < PICSIZE; j++)
 	{
 		peaks[0][j] = 0; // top edge
 		peaks[PICSIZE-1][j] = 0; // bottom edge
+
+		thresh[0][j] = 0.0; // top edge
+		thresh[PICSIZE-1][j] = 0.0; // bottom edge
 	}
 	for(i = 1; i < PICSIZE-1; i++)
 	{
 		for(j = 1; j < PICSIZE-1; j++)
 		{
 			theta = gradDir[i][j];
-			if(theta > 3*M_PI/8 && theta <= -3*M_PI/8) // NS
+			if(( (theta > 67.5) && (theta < 112.5) ) || ( (theta < -67.5) && (theta > -112.5) )) // NS
 			{
 				if(ival[i][j-1] > ival[i][j] || ival[i][j+1] > ival[i][j]) peaks[i][j] = 0;
 				else peaks[i][j] = 1;
 			}
-			else if(theta > M_PI/8 && theta <= 3*M_PI/8) // NE/SW
+			if(( (theta > 22.5) && (theta < 67.5) ) || ( (theta < -112.5) && (theta > -157.5) )) // NE/SW
 			{
 				if(ival[i-1][j-1] > ival[i][j] || ival[i+1][j+1] > ival[i][j]) peaks[i][j] = 0;
 				else peaks[i][j] = 1;
 			}
-			else if(theta > -1*M_PI/8 && theta <= M_PI/8) // EW
+			if(( (theta < 22.5) && (theta > -22.5) ) || (theta > 157.5) || (theta < -157.5)) // EW
 			{
 				if(ival[i-1][j] > ival[i][j] || ival[i+1][j] > ival[i][j]) peaks[i][j] = 0;
 				else peaks[i][j] = 1;
 			}
-			else if(theta > -1*3*M_PI/8 && theta <= -1*M_PI/8) // SE/NW
+			if(( (theta > 112.5) && (theta < 157.5) ) || ( (theta < -22.5) && (theta > -67.5) )) // SE/NW
 			{
 				if(ival[i-1][j+1] > ival[i][j] || ival[i+1][j-1] > ival[i][j]) peaks[i][j] = 0;
 				else peaks[i][j] = 1;
 			}
+			// making threshold image
+			if(peaks[i][j] == 1 && threshold(ival[i][j]) >= 0)
+				thresh[i][j] = 255.0;
+			else thresh[i][j] = 0.0;
 
 		}// end j for
 	}// end i for
 } // end candidates
-
-int getDirection(){
-	int result = 0;
-
-
-	return result;
-}
 
 void normalize(double maxVal)
 {
@@ -209,6 +203,19 @@ void imageToFile(char *file, int output[256][256]){
 			{
 				if(isPeaks) fprintf(fOut,"%c", ((char)(output[i][j] * 255)));
 				else fprintf(fOut,"%c", ((char)output[i][j]));
+			}
+	}
+} // end imageToFile
+void magToFile(char *file, double output[256][256])
+{
+	int i, j;
+
+	FILE *fOut = fopen(file, "wb");
+	fprintf(fOut,"P5\n256 256\n255\n");  /* Print out the .pgm header */
+	for (i=0;i<256;i++)
+	{ for (j=0;j<256;j++)
+			{
+				fprintf(fOut,"%c", ((char)((int)output[i][j])));
 			}
 	}
 } // end imageToFile
